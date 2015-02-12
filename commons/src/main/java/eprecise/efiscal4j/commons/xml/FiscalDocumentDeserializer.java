429d29776b14b9366ed0cc36c903ad1d25ca09ed
veere-carlos <carlos.gomes@veere.com.br>
2015-02-12 16:03:17 -0200

    Correção na validação de Import de CTe

--- commons/src/main/java/eprecise/efiscal4j/commons/xml/FiscalDocumentDeserializer.java
+++ commons/src/main/java/eprecise/efiscal4j/commons/xml/FiscalDocumentDeserializer.java
@@ -37,10 +37,14 @@ public class FiscalDocumentDeserializer {
 	try {
 	    final JAXBContext jaxbContext = JAXBContext.newInstance(entityClass);
 	    final Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-	    return entityClass.cast(unmarshaller.unmarshal(new StringReader(this.xmlContent)));
+	    return entityClass.cast(unmarshaller.unmarshal(new StringReader(this.getPreparedXML())));
 	} catch (final JAXBException e) {
 	    throw new RuntimeException(e);
 	}
+    }
+    
+    private String getPreparedXML() {
+	return this.xmlContent.replace("xmlns=\"http://www.portalfiscal.inf.br/cte\"", "");
     }
     
 }
