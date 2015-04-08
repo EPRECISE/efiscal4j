593af4614cfa82375b4517b0988024983557a640
Fernando Glizt <fernando@veere.com.br>
2015-04-08 19:00:58 -0300

    adequação de enum, possibilitando busca por codigo

--- nfe/src/main/java/eprecise/efiscal4j/nfe/tax/icms/BCModality.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/tax/icms/BCModality.java
@@ -10,37 +10,57 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * Modalidade de determinação da BC do ICMS:
- * 
+ *
  * 0 - Margem Valor Agregado (%);
- * 
+ *
  * 1 - Pauta (valor);
- * 
+ *
  * 2 - Preço Tabelado Máximo (valor);
- * 
+ *
  * 3 - Valor da Operação.
- * 
+ *
  * @author Clécius J. Martinkoski
- * 
+ *
  */
 @XmlType
 @XmlEnum(Integer.class)
 public enum BCModality implements Serializable {
 
-	@XmlEnumValue("0") MARGEM_VALOR_AGREGADO("Margem Valor Agregado (%)"),
-	@XmlEnumValue("1") PAUTA("Pauta (valor)"),
-	@XmlEnumValue("2") PRECO_TABELADO_MAX("Preço Tabelado Máximo (valor)"),
-	@XmlEnumValue("3") VALOR_OPERACAO("Valor da Operação");
+    @XmlEnumValue("0") MARGEM_VALOR_AGREGADO("0", "Margem Valor Agregado (%)"),
+    @XmlEnumValue("1") PAUTA("1", "Pauta (valor)"),
+    @XmlEnumValue("2") PRECO_TABELADO_MAX("2", "Preço Tabelado Máximo (valor)"),
+    @XmlEnumValue("3") VALOR_OPERACAO("3", "Valor da Operação");
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final String description;
+    private final String value;
 
-	private BCModality(String description) {
-		this.description = description;
-	}
+    private final String description;
 
-	public String getDescription() {
-		return this.description;
-	}
+    private BCModality(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBCModalityWithDescription() {
+        return value + " - " + description;
+    }
+
+    public static BCModality findByCode(String code) {
+        for (final BCModality bcModality : values()) {
+            if (bcModality.getValue().equals(code)) {
+                return bcModality;
+            }
+        }
+        return null;
+    }
 
 }
--- nfe/src/main/java/eprecise/efiscal4j/nfe/tax/icms/BCModalityST.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/tax/icms/BCModalityST.java
@@ -10,41 +10,62 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * Modalidade de determinação da BC do ICMS ST:
- * 
+ *
  * 0 – Preço tabelado ou máximo sugerido;
- * 
+ *
  * 1 - Lista Negativa (valor);
- * 
+ *
  * 2 - Lista Positiva (valor);
- * 
+ *
  * 3 - Lista Neutra (valor);
- * 
+ *
  * 4 - Margem Valor Agregado (%);
- * 
+ *
  * 5 - Pauta (valor);
- * 
+ *
  * @author Clécius J. Martinkoski
- * 
+ *
  */
 @XmlType
 @XmlEnum(Integer.class)
 public enum BCModalityST implements Serializable {
-	@XmlEnumValue("0") PRECO_TABELADO_OU_MAX_SUGERIDO("Preço tabelado ou máximo sugerido"),
-	@XmlEnumValue("1") LISTA_NEGATIVA("Lista Negativa"),
-	@XmlEnumValue("2") LISTA_POSITIVA("Lista Positiva"),
-	@XmlEnumValue("3") LISTA_NEUTRA("Lista Neutra "),
-	@XmlEnumValue("4") MARGEM_VALOR_AGREGADO("Margem Valor Agregado"),
-	@XmlEnumValue("5") PAUTA("Pauta (valor)");
+    @XmlEnumValue("0") PRECO_TABELADO_OU_MAX_SUGERIDO("0", "Preço tabelado ou máximo sugerido"),
+    @XmlEnumValue("1") LISTA_NEGATIVA("1", "Lista Negativa"),
+    @XmlEnumValue("2") LISTA_POSITIVA("2", "Lista Positiva"),
+    @XmlEnumValue("3") LISTA_NEUTRA("3", "Lista Neutra "),
+    @XmlEnumValue("4") MARGEM_VALOR_AGREGADO("4", "Margem Valor Agregado"),
+    @XmlEnumValue("5") PAUTA("5", "Pauta (valor)");
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private final String description;
+    private final String value;
 
-	private BCModalityST(String description) {
-		this.description = description;
-	}
+    private final String description;
 
-	public String getDescription() {
-		return this.description;
-	}
+    private BCModalityST(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBCModalitySTWithDescription() {
+        return value + " - " + description;
+    }
+
+    public static BCModalityST findByCode(String code) {
+        for (final BCModalityST bcModalityST : values()) {
+            if (bcModalityST.getValue().equals(code)) {
+                return bcModalityST;
+            }
+        }
+        return null;
+    }
+
 }
