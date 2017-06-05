424c962ae1801c2330efc5f097537b756d1d7639
Fernando C Glizt <fernando@e-precise.com.br>
2017-06-05 18:46:02 -0300

    modified:   nfse/src/main/java/eprecise/efiscal4j/nfse/domain/service/emitter/NFSeServiceEmitter.java

--- nfse/src/main/java/eprecise/efiscal4j/nfse/domain/service/emitter/NFSeServiceEmitter.java
+++ nfse/src/main/java/eprecise/efiscal4j/nfse/domain/service/emitter/NFSeServiceEmitter.java
@@ -3,23 +3,13 @@ package eprecise.efiscal4j.nfse.domain.service.emitter;
 
 import eprecise.efiscal4j.commons.utils.ValidationBuilder;
 import eprecise.efiscal4j.nfse.domain.person.NFSePerson;
-import eprecise.efiscal4j.nfse.domain.service.emitter.specificData.NFSeServiceEmitterSpecificData;
 
 
 public class NFSeServiceEmitter extends NFSePerson {
 
     private static final long serialVersionUID = 1L;
 
-    private final NFSeServiceEmitterSpecificData specificData;
-
     public static class Builder extends NFSePerson.Builder<Builder> {
-
-        private NFSeServiceEmitterSpecificData specificData;
-
-        public Builder withSpecificData(final NFSeServiceEmitterSpecificData specificData) {
-            this.specificData = specificData;
-            return this;
-        }
 
         public NFSeServiceEmitter build() {
             final NFSeServiceEmitter entity = new NFSeServiceEmitter(this);
@@ -29,16 +19,10 @@ public class NFSeServiceEmitter extends NFSePerson {
     }
 
     public NFSeServiceEmitter() {
-        specificData = null;
     }
 
     public NFSeServiceEmitter(final Builder builder) {
         super(builder);
-        specificData = builder.specificData;
-    }
-
-    public NFSeServiceEmitterSpecificData getSpecificData() {
-        return specificData;
     }
 
 }
