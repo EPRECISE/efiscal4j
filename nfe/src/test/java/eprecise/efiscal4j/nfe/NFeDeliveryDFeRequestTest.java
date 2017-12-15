21a5ebdca3340333b81c70af7488008caccdca8a
Luan Bukowitz Beluzzo <luan@e-precise.com.br>
2017-12-15 09:29:58 -0200

    Manifestação destinatário - Teste envio Requisição de informção de DF-e

--- nfe/src/test/java/eprecise/efiscal4j/nfe/NFeDeliveryDFeRequestTest.java
+++ nfe/src/test/java/eprecise/efiscal4j/nfe/NFeDeliveryDFeRequestTest.java
@@ -1,9 +1,13 @@
 
 package eprecise.efiscal4j.nfe;
 
+import javax.validation.ConstraintViolationException;
+
 import org.junit.Test;
 
+import eprecise.efiscal4j.commons.domain.transmission.TypedTransmissionResult;
 import eprecise.efiscal4j.nfe.deliveryDFe.NFeDeliveryDFeRequest;
+import eprecise.efiscal4j.nfe.deliveryDFe.response.NFeDeliveryDFeResponse;
 import eprecise.efiscal4j.nfe.domain.NFeDeliveryDFeDomain;
 import eprecise.efiscal4j.nfe.domain.TestDomain;
 
@@ -12,7 +16,21 @@ public class NFeDeliveryDFeRequestTest implements Testable<NFeDeliveryDFeRequest
 
     private final TestDomain nFeDomain = new TestDomain(NFeDeliveryDFeRequest.XSD);
 
-    private final NFeDeliveryDFeDomain domain = new NFeDeliveryDFeDomain();;
+    private final NFeDeliveryDFeDomain domain = new NFeDeliveryDFeDomain();
+
+    @Test
+    public void transmit() throws Exception {
+        try {
+            final TypedTransmissionResult<NFeDeliveryDFeRequest, NFeDeliveryDFeResponse> result = this.nFeDomain.getTransmissionChannel()
+                    .transmitNFeDeliveryDFe(this.domain.buildQueryAccesKeyRequest());
+
+            result.getResponse();
+
+            System.out.println(result.getResponseXml());
+        } catch (final ConstraintViolationException e) {
+            this.handleErrors(e);
+        }
+    }
 
     @Test
     public void validateByBeanValidation() throws Exception {
