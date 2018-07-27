d88d95d76fba17a637dd7f655483c33d7ac8b3fc
Israel Andrade <israel@e-precise.com.br>
2018-07-27 16:53:05 -0300

    Adapter carta de correção da NFe 4 - dominio intermediario para especifico

--- nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/adapters/dispatchFromFiscalDocument/EventDispatchCCeAdapter.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/adapters/dispatchFromFiscalDocument/EventDispatchCCeAdapter.java
@@ -1,19 +1,73 @@
+
 package eprecise.efiscal4j.nfe.v400.sharing.adapters.dispatchFromFiscalDocument;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Optional;
+
+import eprecise.efiscal4j.commons.domain.FiscalDocumentVersion;
+import eprecise.efiscal4j.nfe.FiscalDocument;
 import eprecise.efiscal4j.nfe.FiscalDocumentCCe;
+import eprecise.efiscal4j.nfe.emitter.documents.EmitterLegalEntityDocuments;
+import eprecise.efiscal4j.nfe.emitter.documents.EmitterNaturalPersonDocuments;
+import eprecise.efiscal4j.nfe.v400.TransmissionEnvironment;
+import eprecise.efiscal4j.nfe.v400.address.IBGEOrgan;
+import eprecise.efiscal4j.nfe.v400.sharing.Event;
+import eprecise.efiscal4j.nfe.v400.sharing.EventDetailCCe;
 import eprecise.efiscal4j.nfe.v400.sharing.EventDispatch;
+import eprecise.efiscal4j.nfe.v400.sharing.EventInfo;
+import eprecise.efiscal4j.nfe.v400.sharing.EventType;
 import eprecise.efiscal4j.nfe.version.EventDispatchCCeVersion;
+import eprecise.efiscal4j.signer.defaults.DefaultSigner;
 
-public class EventDispatchCCeAdapter implements EventDispatchCCeVersion{
 
-	private final FiscalDocumentCCe fiscalDocumentCCe;
+public class EventDispatchCCeAdapter implements EventDispatchCCeVersion {
 
-	public EventDispatchCCeAdapter(FiscalDocumentCCe fiscalDocumentCCe) {
-		this.fiscalDocumentCCe = fiscalDocumentCCe;
-	}
+    private static final DateFormat NFE_DATETIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 
-	public EventDispatch buildEventDispatchCCe() {
-		return null;
-	}
+    private static final String NFE_EVENT_SEQ_NUMBER = "1";
 
+    private final FiscalDocumentCCe fiscalDocumentCCe;
+
+    public EventDispatchCCeAdapter(FiscalDocumentCCe fiscalDocumentCCe) {
+        this.fiscalDocumentCCe = fiscalDocumentCCe;
+    }
+
+    @Override
+    public EventDispatch buildEventDispatchCCe() {
+     // @formatter:off 
+        try {
+    		return new EventDispatch.Builder()
+    		        .withBatchId(Optional.ofNullable(this.fiscalDocumentCCe.getProcessedFiscalDocument().getDocument()).map(d -> d.getNumber().toString()).orElse(null))
+    		        .withEvents(Arrays.asList(new Event.Builder()
+                            .withEventInfo(this.buildEventInfo())
+                            .build(new DefaultSigner(Optional.ofNullable(this.fiscalDocumentCCe.getProcessedFiscalDocument().getDocument()).map(d -> d.getEmitter().getCertificate()).orElse(null)))))
+    		        .build();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+     // @formatter:on	
+    }
+
+    private EventInfo buildEventInfo() {
+        // @formatter:off    
+           final Optional<FiscalDocument> fiscalDocument = Optional.ofNullable(this.fiscalDocumentCCe.getProcessedFiscalDocument().getDocument());
+           return new EventInfo.Builder()
+                   .withIbgeOrgan(IBGEOrgan.findByAcronym(fiscalDocument.map(d -> d.getEmitter().getAddress().getCity().getUf().getAcronym()).orElse(null)))
+                   .withTransmissionEnvironment(TransmissionEnvironment.findBy(fiscalDocument.map(d -> d.getSerie().getEnvironment().getValue()).orElse(null)).orElse(null))
+                   .withAuthorCnpj(fiscalDocument.map(d -> d.getEmitter().getDocuments()).filter(EmitterLegalEntityDocuments.class::isInstance).map(led -> led.getCnp()).orElse(null))
+                   .withAuthorCpf(fiscalDocument.map(d -> d.getEmitter().getDocuments()).filter(EmitterNaturalPersonDocuments.class::isInstance).map(npd -> npd.getCnp()).orElse(null))
+                   .withAcessKey(this.fiscalDocumentCCe.getProcessedFiscalDocument().getAccessKey())
+                   .withEventDateTime(EventDispatchCCeAdapter.NFE_DATETIME_FORMAT.format(new Date()))
+                   .withEventType(EventType.CANC_NFE)
+                   .withEventSeqNumber(EventDispatchCCeAdapter.NFE_EVENT_SEQ_NUMBER)
+                   .withEventVersion(FiscalDocumentVersion.VERSION_1_00.getValue())
+                   .withEventDetail(new EventDetailCCe.Builder()
+                           .withCorrection(this.fiscalDocumentCCe.getCorrection())
+                           .build())
+                   .build();
+        // @formatter:on    
+    }
 }
