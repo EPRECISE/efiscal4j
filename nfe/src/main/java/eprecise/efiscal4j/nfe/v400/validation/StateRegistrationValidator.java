a54252fdca39d4a8e3d43f0da7284e92ac317188
Fernando C. Glizt <fernando@e-precise.com.br>
2019-03-21 18:02:56 -0300

    adequações em adapter e validação de indicador de IE para suportar destinatário com id estrangeiro

--- nfe/src/main/java/eprecise/efiscal4j/nfe/v400/validation/StateRegistrationValidator.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/v400/validation/StateRegistrationValidator.java
@@ -1,10 +1,14 @@
 
 package eprecise.efiscal4j.nfe.v400.validation;
 
+import java.util.Optional;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import eprecise.efiscal4j.commons.domain.adress.UF;
 import eprecise.efiscal4j.nfe.v400.StateRegistrationReceiverIndicator;
+import eprecise.efiscal4j.nfe.v400.address.City;
 import eprecise.efiscal4j.nfe.v400.person.Receiver;
 
 
@@ -22,8 +26,10 @@ public class StateRegistrationValidator implements ConstraintValidator<StateRegi
 
     @Override
     public boolean isValid(Receiver receiver, ConstraintValidatorContext context) {
-        //@formatter:off      	    
-        if ((receiver.getStateRegistrationReceiverIndicator() == null) 
+        //@formatter:off
+        if(Optional.ofNullable(receiver.getAdress()).map(r->r.getCity()).map(City::getUf).filter(UF.EX::equals).isPresent()) {
+            return true;
+        } else if ((receiver.getStateRegistrationReceiverIndicator() == null) 
                 || (receiver.getStateRegistrationReceiverIndicator().equals(StateRegistrationReceiverIndicator.CONTRIBUINTE_ICMS) 
                         && receiver.getDocuments().getStateRegistration() == null)) { 
             return false; 
