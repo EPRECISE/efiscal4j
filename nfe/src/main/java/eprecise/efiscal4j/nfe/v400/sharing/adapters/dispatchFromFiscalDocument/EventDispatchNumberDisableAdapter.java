21895bdc2b270fccf933abfe583fb1d04e31715a
Fernando C. Glizt <fernando@e-precise.com.br>
2019-05-08 08:45:54 -0300

    modified:   nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/adapters/dispatchFromFiscalDocument/EventDispatchNumberDisableAdapter.java

--- nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/adapters/dispatchFromFiscalDocument/EventDispatchNumberDisableAdapter.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/adapters/dispatchFromFiscalDocument/EventDispatchNumberDisableAdapter.java
@@ -4,7 +4,9 @@ package eprecise.efiscal4j.nfe.v400.sharing.adapters.dispatchFromFiscalDocument;
 import eprecise.efiscal4j.commons.utils.Certificate;
 import eprecise.efiscal4j.nfe.FiscalDocumentNumberDisable;
 import eprecise.efiscal4j.nfe.v400.sharing.NFeNumberDisableDispatch;
+import eprecise.efiscal4j.nfe.v400.sharing.NFeNumberDisableInfo;
 import eprecise.efiscal4j.nfe.version.EventDispatchNumberDisableVersion;
+import eprecise.efiscal4j.signer.defaults.DefaultSigner;
 
 
 public class EventDispatchNumberDisableAdapter implements EventDispatchNumberDisableVersion {
@@ -20,6 +22,17 @@ public class EventDispatchNumberDisableAdapter implements EventDispatchNumberDis
 
     @Override
     public NFeNumberDisableDispatch buildEventDispatchNumberDisable() {
+        try {
+            return new NFeNumberDisableDispatch.Builder()
+                    .withInfo(buildNFeInfo())
+                    .build(new DefaultSigner(this.certificate));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private NFeNumberDisableInfo buildNFeInfo() {
+        // TODO Auto-generated method stub
         return null;
     }
 
