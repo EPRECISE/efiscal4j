b949cf46f06eb4b2071974eadfa93a4eb489da28
Fernando C. Glizt <fernando@e-precise.com.br>
2019-06-13 08:04:41 -0300

    alterado soapaction evento cancelamento e carta de correcao

--- nfe/src/main/java/eprecise/efiscal4j/nfe/v400/transmission/TransmissionChannel.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/v400/transmission/TransmissionChannel.java
@@ -189,7 +189,7 @@ public class TransmissionChannel implements NFeTransmissionChannel {
         final String requestXml = new FiscalDocumentSerializer<>(eventDispatch).serialize();
 
         String responseXml = this.transmissor.transmit(new FiscalDocumentSerializer<>(soapEnvelope).serialize(), serviceUrl,
-                ImmutableMap.of("SOAPAction", "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4/nfeRecepcaoEventoNF"));
+                ImmutableMap.of("SOAPAction", "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4/nfeRecepcaoEvento"));
 
         responseXml = this.postProcessResponseXML(responseXml);
 
@@ -225,7 +225,7 @@ public class TransmissionChannel implements NFeTransmissionChannel {
         final String requestXml = new FiscalDocumentSerializer<>(eventDispatch).serialize();
 
         String responseXml = this.transmissor.transmit(new FiscalDocumentSerializer<>(soapEnvelope).serialize(), serviceUrl,
-                ImmutableMap.of("SOAPAction", "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4/nfeRecepcaoEventoNF"));
+                ImmutableMap.of("SOAPAction", "http://www.portalfiscal.inf.br/nfe/wsdl/NFeRecepcaoEvento4/nfeRecepcaoEvento"));
 
         responseXml = this.postProcessResponseXML(responseXml);
 
