6456d9d00b94b653ae174f21c5cfa46a4d14ef53
Fernando C. Glizt <fernando@e-precise.com.br>
2019-07-10 16:35:48 -0300

    ajustado ordem de atributos

--- nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/NFeDispatchResponse.java
+++ nfe/src/main/java/eprecise/efiscal4j/nfe/v400/sharing/NFeDispatchResponse.java
@@ -36,9 +36,9 @@ public class NFeDispatchResponse implements Serializable {
 
     public static final String XSD = "/eprecise/efiscal4j/nfe/v400/xsd/retEnviNFe_v4.00.xsd";
 
-    private @XmlAttribute(name = "versao") @NotNull final FiscalDocumentVersion version = FiscalDocumentVersion.VERSION_4_00;
-
     private @XmlAttribute(name = "xmlns") final String xmlns = "http://www.portalfiscal.inf.br/nfe";
+
+    private @XmlAttribute(name = "versao") @NotNull final FiscalDocumentVersion version = FiscalDocumentVersion.VERSION_4_00;
 
     private @XmlElement(name = "tpAmb") @Valid @NotNull final TransmissionEnvironment transmissionEnvironment;
 
